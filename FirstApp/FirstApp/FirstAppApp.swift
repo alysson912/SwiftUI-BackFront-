1a469eeeb763a68d0e387b1df87960dad77563f0
alysson912 <almenezes912@gmail.com>
2023-10-24 13:04:52 -0300

    Fist commit project

---
+++ FirstApp/FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Alysson Menezes on 24/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
